0897919de47f78fdeebadad605b52c62c35f1875
JamesGEllis <me@jamesellis.ca>
2024-02-14 11:17:06 -0500

    [#181909930] Added void typehint to `CrawlerDetectExtension::load()`.

--- DependencyInjection/CrawlerDetectExtension.php
+++ DependencyInjection/CrawlerDetectExtension.php
@@ -12,7 +12,7 @@ class CrawlerDetectExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container) : void
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
